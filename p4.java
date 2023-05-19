cd58fc033b9dae4f31dbe2ac0aad1f6392f0cdbc
Ayush Gupta <ayush@Ayushs-MacBook-Air.local>
2023-05-19 23:29:12 +0530

    p

---
+++ p4.java
@@ -0,0 +1,19 @@
+package Pattern;
+import java.util.Scanner;
+public class p4 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			
+			
+			for(int j=i+1;j<=6;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
